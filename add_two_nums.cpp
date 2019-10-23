c510fdd3f1ea42c0a8b50c323aa69d11a5a45e8d
madhumithanara <madhumithanara.171it125@nitk.edu.in>
2019-10-23 13:45:08 +0530

    Added basic programs

---
+++ add_two_nums.cpp
@@ -0,0 +1,15 @@
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter the 1st number\n";
+    cin>>a;
+    cout<<"Enter the 2nd number\n";
+    cin>>b;
+    sum=a+b;
+    cout<<"Sum of two number "<<sum;
+    return 0;
+}
---
+++ if_else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"Enter the first number\t";
+cin>>a;
+cout<<"Enter the second number\t";
+cin>>b;
+if(a>b)
+cout<<"Greatest number is\t"<<a;
+else
+cout<<"Greatest number is\t"<<b;
+return 0;
+}
